75397c29399714174fde71bcb5550e3defeb9cc3
djane <d.mabelin@arcanys.com>
2020-07-24 09:48:46 +0800

    fix font path

--- overlays/constants.py
+++ overlays/constants.py
@@ -1,4 +1,4 @@
-import os
-FONT = os.path.abspath('./fonts/data-latin.ttf')
+from os import path
+FONT = path.join(path.dirname(__file__), '../fonts/data-latin.ttf')
 COCKPIT_COLOR = (254, 132, 0)
 MARGIN = 10
