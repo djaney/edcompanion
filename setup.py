ea30b460011bd1911d3839cef699f9adaf184f69
Djane Rey Mabelin <djaney@users.noreply.github.com>
2020-08-10 16:47:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if os.name == 'nt':
 
 setup(
     name='edcompanion',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/djaney/edcompanion',
     author='Djane Rey Mabelin',
     author_email='thedjaney@gmail.com',
