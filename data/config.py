a551312e0b28afb22c1e53ca126ce08c4776bea2
djane <d.mabelin@arcanys.com>
2020-08-20 08:38:30 +0800

    simple racing card

--- data/config.py
+++ data/config.py
@@ -1,14 +1,21 @@
 import platform
 import os
+import json
 
 
-class Config():
+class Config(object):
+    RACES_DIR = 'races'
+    MAIN_CONFIG = 'config.json'
+    CONFIG_ITEMS_MAP = {
+        "edsmApiKey": "edsm_api_key"
+    }
+
+    __config = {}
 
     def __init__(self, config_dir=None):
         if config_dir is None:
             if platform.system().lower() == 'linux':
-                self.dir = '/home/{}/.local/share/Steam/steamapps/compatdata/359320/pfx/drive_c/users/steamuser/' \
-                              'Saved Games/edcompanion'.format(os.environ.get('USER', ''))
+                self.dir = '/home/{}/.edcompanion'.format(os.environ.get('USER', ''))
             elif platform.system().lower() == 'windows':
                 self.dir = '{}\\Saved Games\\edcompanion'.format(os.environ.get('USERPROFILE', ''))
             else:
@@ -17,6 +24,49 @@ class Config():
         if not os.path.isdir(self.dir):
             os.makedirs(self.dir, exist_ok=True)
 
-    def get_race(self):
+        os.makedirs(os.path.join(self.dir, self.RACES_DIR), exist_ok=True)
+
+    def set(self, key, value):
+        if key not in self.CONFIG_ITEMS_MAP.values():
+            raise KeyError("key not in {}".format(",".join(self.CONFIG_ITEMS_MAP.values())))
+
+        self.__config[key] = value
+        self.save()
+
+    def get(self, key, default=None):
+        if key not in self.CONFIG_ITEMS_MAP.values():
+            raise KeyError("key not in {}".format(",".join(self.CONFIG_ITEMS_MAP.values())))
+        self.load()
+        return self.__config.get(key, default)
+
+    def save(self):
+
+        data = {}
+        for file_field, class_field in self.CONFIG_ITEMS_MAP.items():
+            if class_field in self.__config:
+                data[file_field] = self.__config[class_field]
+
+        filename = os.path.join(self.dir, self.MAIN_CONFIG)
+        with open(filename, 'w') as fp:
+            json.dump(data, fp)
+
+    def load(self):
+        filename = os.path.join(self.dir, self.MAIN_CONFIG)
+        if os.path.isfile(filename):
+            with open(filename, 'r') as fp:
+                data = json.load(fp)
+            for file_field, class_field in self.CONFIG_ITEMS_MAP.items():
+                if file_field in data:
+                    self.__config[class_field] = data[file_field]
+
+    def select_race(self):
+        # todo
+        pass
+
+    def get_race_details(self):
+        # todo
+        pass
+
+    def get_races(self):
         # todo
         pass
--- scripts/launcher.py
+++ scripts/launcher.py
@@ -3,36 +3,49 @@ from overlays.constants import TITLE
 from scripts import edcompanion
 
 
-def open_companion(window, *args, options=None):
+def open_companion(*args, options=None):
     args = list(args)
     if options is not None:
         if 'is_overlay' in options and options['is_overlay'].get() == 1:
             args.append('--overlay')
 
     def open_window():
-        window.destroy()
         edcompanion.main(*args)
 
     return open_window
 
 
 def main():
-    window = tk.Tk()
+    root = tk.Tk()
+    root.title(TITLE)
+    root.geometry("300x50")
 
-    window.title(TITLE)
+    mode_frame = tk.LabelFrame(text="Modes")
+    mode_frame.pack(side="top", expand="yes", fill="x")
 
+    layout_ltr = dict(side='left', expand="yes", fill="x")
+
+    # overlay checkbox
     is_overlay = tk.IntVar(value=1)
+    tk.Checkbutton(mode_frame, text="Overlay", variable=is_overlay).pack(side='left')
 
-    chk_overlay = tk.Checkbutton(window, text="Overlay", variable=is_overlay)
+    # exploration button
+    tk.Button(
+        mode_frame, text="Exploration",
+        command=open_companion(
+            'exploration',
+            options={"is_overlay": is_overlay}
+        )
+    ).pack(**layout_ltr)
 
-    options = dict(is_overlay=is_overlay)
+    # exploration button
+    tk.Button(
+        mode_frame, text="Race",
+        command=open_companion('race')
+    ).pack(**layout_ltr)
 
-    btn_exploration = tk.Button(window, text="Exploration", command=open_companion(window, 'exploration',
-                                                                                   options=options))
-
-    chk_overlay.pack()
-    btn_exploration.pack()
-    window.mainloop()
+    # main loop
+    root.mainloop()
 
 
 if __name__ == "__main__":
